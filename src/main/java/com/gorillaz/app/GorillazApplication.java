887334a975bed5b3a1825acd5948b7a73eef0ae2
Luiz Fernando <luiz.fpimentel0@gmail.com>
2024-05-23 01:44:04 -0300

    chore: setting project setup

---
+++ src/main/java/com/gorillaz/app/GorillazApplication.java
@@ -0,0 +1,13 @@
+package com.gorillaz.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GorillazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GorillazApplication.class, args);
+	}
+
+}
