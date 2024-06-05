0548717582bb8df82bfde954594339a1f5c16ab4
Luiz Fernando <luiz.fpimentel0@gmail.com>
2024-06-05 01:48:04 -0300

    feat: add auth to UserController and change the endpoint to /users

--- src/main/java/com/gorillaz/app/controller/UserController.java
+++ src/main/java/com/gorillaz/app/controller/UserController.java
@@ -1,33 +1,55 @@
 package com.gorillaz.app.controller;
 
-import com.gorillaz.app.model.User;
+import com.gorillaz.app.config.security.TokenService;
+import com.gorillaz.app.domain.user.AuthenticationDTO;
+import com.gorillaz.app.domain.user.LoginResponseDTO;
+import com.gorillaz.app.domain.user.RegisterDTO;
+import com.gorillaz.app.domain.user.User;
+import com.gorillaz.app.service.AuthService;
 import com.gorillaz.app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/users")
 public class UserController {
     @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
     private UserService userService;
+    @Autowired
+    private TokenService tokenService;
 
-    @GetMapping("users/all")
-    public ResponseEntity<List<User>> findAll() {
-        List<User> user = userService.findAll();
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Validated RegisterDTO data) {
+        if (this.userService.findUserByEmail(data.email()) != null)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("E-mail utilizado por outro usuário.");
 
-        return ResponseEntity.ok(user);
+        String passwordHashed = new BCryptPasswordEncoder().encode(data.password());
+        User user = new User(data.name(), data.email(), passwordHashed, data.gender(), data.course(), data.role());
+
+        var userCreated = this.userService.save(user);
+
+        if (userCreated == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        System.out.println("usuario " + user.toString());
-        User userCreated = userService.save(user);
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Validated AuthenticationDTO data) {
+        var emailPassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
+        var auth = this.authenticationManager.authenticate(emailPassword);
 
-        return ResponseEntity.ok(userCreated);
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginResponseDTO(token));
     }
-
 }
