e698930d6903bc7d20d4601f0c41bc245a45602e
Luiz Fernando <luiz.fpimentel0@gmail.com>
2024-06-08 01:06:22 -0300

    feat(RegisterDTO): add other fields

--- src/main/java/com/gorillaz/app/domain/user/RegisterDTO.java
+++ src/main/java/com/gorillaz/app/domain/user/RegisterDTO.java
@@ -1,7 +1,8 @@
 package com.gorillaz.app.domain.user;
 
 import com.gorillaz.app.enums.Gender;
+import com.gorillaz.app.enums.Period;
 import com.gorillaz.app.enums.UserRole;
 
-public record RegisterDTO(String name, String email, String password, Gender gender, String course, UserRole role) {
+public record RegisterDTO(String name, String email, String password, String ra, Gender gender, String course, Period period, boolean isRepresentative, UserRole role) {
 }
