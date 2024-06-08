f56a82c533ff72629dd8806c9e171a7ff37a0bc3
Luiz Fernando <luiz.fpimentel0@gmail.com>
2024-06-08 00:30:42 -0300

    feat(PostController): get current user and save on post

--- src/main/java/com/gorillaz/app/controller/PostController.java
+++ src/main/java/com/gorillaz/app/controller/PostController.java
@@ -2,10 +2,14 @@ package com.gorillaz.app.controller;
 
 import com.gorillaz.app.domain.post.NewPostDTO;
 import com.gorillaz.app.domain.post.Post;
+import com.gorillaz.app.domain.user.User;
 import com.gorillaz.app.service.PostService;
+import com.gorillaz.app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,18 +19,33 @@ public class PostController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private UserService userService;
+
     @PostMapping
     public ResponseEntity newPost(@RequestBody @Validated NewPostDTO data) {
-        // TODO pegar o user atual e atribuir ao post
-        Post post = new Post(data.title(), data.subtitle(), data.text(), data.postDate());
 
-        var postCreated = this.postService.save(post);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
-        if (postCreated == null) {
+        if (auth != null) {
+            String username = auth.getName();
+            User user = userService.getUserByEmail(username);
+
+            if (user != null) {
+                Post post = new Post(data.title(), data.subtitle(), data.text(), data.postDate(), user);
+
+                var postCreated = this.postService.save(post);
+
+                if (postCreated == null) {
+                    return ResponseEntity.badRequest().body("Erro ao criar o post");
+                }
+
+                return ResponseEntity.status(HttpStatus.CREATED).build();
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuário não autenticado");
+            }
+        } else
             return ResponseEntity.badRequest().build();
-        }
-
-        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping
