90538feee17d11cd957877f1f527877db2c941aa
Luiz Fernando <luiz.fpimentel0@gmail.com>
2024-06-08 01:44:39 -0300

    feat(UserService): create method to get the authenticated user

--- src/main/java/com/gorillaz/app/service/UserService.java
+++ src/main/java/com/gorillaz/app/service/UserService.java
@@ -2,6 +2,8 @@ package com.gorillaz.app.service;
 
 import com.gorillaz.app.domain.user.User;
 import com.gorillaz.app.repository.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -24,4 +26,14 @@ public class UserService {
    public User getUserByEmail (String email) {
       return (User) userRepository.findByEmail(email);
    }
+
+    public User getAuthenticatedUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth == null) {
+            return null;
+        }
+
+        return this.getUserByEmail(auth.getName());
+    }
 }
