37e671cba37eb015e026114b64fd6c1c8d7af21c
Luiz Fernando <luiz.fpimentel0@gmail.com>
2024-06-11 18:52:42 -0300

    fix(User): map dto to receive semester

--- src/main/java/com/gorillaz/app/domain/user/UserProfileDTO.java
+++ src/main/java/com/gorillaz/app/domain/user/UserProfileDTO.java
@@ -3,5 +3,5 @@ package com.gorillaz.app.domain.user;
 import com.gorillaz.app.enums.Gender;
 import com.gorillaz.app.enums.Period;
 
-public record UserProfileDTO(String name, String email, Gender gender, String ra, String course, Period period, Integer semester, boolean representative) {
+public record UserProfileDTO(String name, String email, Gender gender, String ra, String course, Period period, int semester, boolean representative) {
 }
