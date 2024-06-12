1f418a4f3c125f1bf345b92324e8ffa363707028
Luiz Fernando <luiz.fpimentel0@gmail.com>
2024-06-11 23:01:15 -0300

    feat(RegistrationRepository): create repository

---
+++ src/main/java/com/gorillaz/app/repository/RegistrationRepository.java
@@ -0,0 +1,14 @@
+package com.gorillaz.app.repository;
+
+import com.gorillaz.app.domain.event.Event;
+import com.gorillaz.app.domain.event.Registration;
+import com.gorillaz.app.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface RegistrationRepository extends JpaRepository<Registration, UUID> {
+    boolean existsByUserAndEvent(User user, Event event);
+}
+
+
